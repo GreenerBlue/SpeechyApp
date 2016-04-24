41dedfb2b6e7cccfcde80fbc4bf61ef1c61ab07a
Yashodhan <yashodhanghadge@gmail.com>
2016-04-24 12:41:19 +0530

    added listView support

--- app/src/main/java/com/gears/yashodhan/speechy/musicPlayer.java
+++ app/src/main/java/com/gears/yashodhan/speechy/musicPlayer.java
@@ -8,6 +8,9 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -15,23 +18,21 @@ import java.util.List;
 
 public class musicPlayer extends AppCompatActivity {
 
-    String[] projection = {
-            MediaStore.Audio.Media._ID,
-            MediaStore.Audio.Media.ARTIST,
-            MediaStore.Audio.Media.TITLE,
-            MediaStore.Audio.Media.DATA,
-            MediaStore.Audio.Media.DISPLAY_NAME,
-            MediaStore.Audio.Media.DURATION
-    };
 
-
-    String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
     static private final String TAG = "musicPlayerActivity";
+    private List<String> paths = new ArrayList<String>();
+    private ListView lv;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_player);
+
+        lv = (ListView) findViewById(R.id.listView);
+        adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, paths);
+        lv.setAdapter(adapter);
+
     }
 
     public void searchMusic(View v){
@@ -43,7 +44,14 @@ public class musicPlayer extends AppCompatActivity {
         String sortOrder = MediaStore.Audio.Media.TITLE + " ASC";
         Cursor cur = cr.query(uri, null, selection, null, sortOrder);
         int count = 0;
-        //List<String> paths = new ArrayList<String>();
+
+        if(paths.size()!=0)
+          switch (v.getId()){
+              case R.id.searchMusicBtn:
+
+                  break;
+
+          }
         if(cur != null)
         {
             count = cur.getCount();
@@ -55,6 +63,7 @@ public class musicPlayer extends AppCompatActivity {
                     String data = cur.getString(cur.getColumnIndex(MediaStore.Audio.Media.DATA));
                     // Add code to get more column here
                     //paths.add(data);
+                    adapter.add(data);
                     // Save to your list here
                     Log.d(TAG,data);
                 }
@@ -62,6 +71,8 @@ public class musicPlayer extends AppCompatActivity {
         }
 
         cur.close();
+
+
 
         Toast.makeText(this,"hello",Toast.LENGTH_LONG).show();
     }
